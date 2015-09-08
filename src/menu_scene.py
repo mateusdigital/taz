856f0bdb0a52618b111bbd7efd55544f02f471b0
Mateus Mesquita <n2o.matt@gmail.com>
2015-09-07 21:44:49 -0300

    Fix the name of file in license header. Change the load image to use the resources.Sprites instead of hardcoded filenames. Add some comments in handle_events and put a var to hold the value easing the checking.

--- src/menu_scene.py
+++ src/menu_scene.py
@@ -5,7 +5,7 @@
 ##               ████████                                                     ##
 ##             ██        ██                                                   ##
 ##            ███  █  █  ███                                                  ##
-##            █ █        █ █        splash_scene.py                           ##
+##            █ █        █ █        game_scene.py                             ##
 ##             ████████████         Game Taz                                  ##
 ##           █              █       Copyright (c) 2015 AmazingCow             ##
 ##          █     █    █     █      www.AmazingCow.com                        ##
@@ -44,10 +44,11 @@
 #Pygame
 import pygame;
 #Project
-from game  import Director;
-from scene import Scene;
-from scene import Sprite;
-from clock import BasicClock;
+from game      import Director;
+from scene     import Scene;
+from scene     import Sprite;
+from clock     import BasicClock;
+from resources import Sprites;
 
 class MenuScene(Scene):
     ############################################################################
@@ -59,13 +60,13 @@ class MenuScene(Scene):
         #Init the Sprites....
         #Taz.
         self.taz_logo = Sprite();
-        self.taz_logo.load_image("../resources/Sprite_TazLogo.png");
+        self.taz_logo.load_image(Sprites.TazLogo);
         self.taz_logo.set_position(216, 39);
         self.add(self.taz_logo);
 
         #Amazing Cow Url.
         self.cow_url = Sprite();
-        self.cow_url.load_image("../resources/Sprite_AmazingCowURL.png");
+        self.cow_url.load_image(Sprites.AmazingCowUrl);
         self.cow_url.set_position(225, 397);
         self.add(self.cow_url);
 
@@ -74,11 +75,11 @@ class MenuScene(Scene):
         self.current_index = 0;
         #Play.
         self.play_option = Sprite();
-        self.play_option.load_image("../resources/Sprite_Play.png");
+        self.play_option.load_image(Sprites.MenuPlay);
         self.play_option.set_position(217, 333);
         #Credits.
         self.credits_option = Sprite();
-        self.credits_option.load_image("../resources/Sprite_Credits.png");
+        self.credits_option.load_image(Sprites.MenuCredits);
         self.credits_option.set_position(217, 333);
 
         self.menu_options.append(self.play_option);
@@ -94,12 +95,16 @@ class MenuScene(Scene):
         Scene.draw(self,surface);
 
     def handle_events(self, event):
+        #We're only interested in keydown events.
         if(event.type != pygame.locals.KEYDOWN):
             return;
 
-        if(event.key == pygame.locals.K_UP or event.key == pygame.locals.K_DOWN):
+        key = event.key;
+        #Up and Down change the option.
+        if(key == pygame.locals.K_UP or key == pygame.locals.K_DOWN):
             self.change_menu_option();
-        elif(event.key == pygame.locals.K_RETURN):
+        #Enter commit the selection.
+        elif(key == pygame.locals.K_RETURN):
             self.change_scene();
 
 
