e2f0c7e89f0aa7d3349822d5208a8d8680084623
Mateus Mesquita <n2o.matt@gmail.com>
2015-09-07 21:28:15 -0300

    Add clock class.

---
+++ src/clock.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+#coding=utf8
+##----------------------------------------------------------------------------##
+##               █      █                                                     ##
+##               ████████                                                     ##
+##             ██        ██                                                   ##
+##            ███  █  █  ███                                                  ##
+##            █ █        █ █        clock.py                                  ##
+##             ████████████         Game Taz                                  ##
+##           █              █       Copyright (c) 2015 AmazingCow             ##
+##          █     █    █     █      www.AmazingCow.com                        ##
+##          █     █    █     █                                                ##
+##           █              █       N2OMatt - n2omatt@amazingcow.com          ##
+##             ████████████         www.amazingcow.com/n2omatt                ##
+##                                                                            ##
+##                                                                            ##
+##                  This software is licensed as GPLv3                        ##
+##                 CHECK THE COPYING FILE TO MORE DETAILS                     ##
+##                                                                            ##
+##    Permission is granted to anyone to use this software for any purpose,   ##
+##   including commercial applications, and to alter it and redistribute it   ##
+##               freely, subject to the following restrictions:               ##
+##                                                                            ##
+##     0. You **CANNOT** change the type of the license.                      ##
+##     1. The origin of this software must not be misrepresented;             ##
+##        you must not claim that you wrote the original software.            ##
+##     2. If you use this software in a product, an acknowledgment in the     ##
+##        product IS HIGHLY APPRECIATED, both in source and binary forms.     ##
+##        (See opensource.AmazingCow.com/acknowledgment.html for details).    ##
+##        If you will not acknowledge, just send us a email. We'll be         ##
+##        *VERY* happy to see our work being used by other people. :)         ##
+##        The email is: acknowledgmentopensource@AmazingCow.com               ##
+##     3. Altered source versions must be plainly marked as such,             ##
+##        and must notbe misrepresented as being the original software.       ##
+##     4. This notice may not be removed or altered from any source           ##
+##        distribution.                                                       ##
+##     5. Most important, you must have fun. ;)                               ##
+##                                                                            ##
+##      Visit opensource.amazingcow.com for more open-source projects.        ##
+##                                                                            ##
+##                                  Enjoy :)                                  ##
+##----------------------------------------------------------------------------##
+
+## Imports ##
+#Pygame
+import pygame;
+
+class BasicClock(object):
+    ############################################################################
+    ## CTOR                                                                   ##
+    ############################################################################
+    def __init__(self, time):
+        self.__tick_time            = time;
+        self.__time_since_last_tick = None;
+        self.__enabled              = False;
+
+    ############################################################################
+    ## Set/Get Time                                                           ##
+    ############################################################################
+    def set_time(self, time):
+        self.__time_since_last_tick = 0;
+        self.__tick_time            = time;
+
+    def get_time(self):
+        return self.__tick_time;
+
+    ############################################################################
+    ## Start/Stop/Enabled                                                     ##
+    ############################################################################
+    def start(self):
+        self.__enabled              = True;
+        self.__time_since_last_tick = 0;
+
+    def stop(self):
+        self.__enabled = False;
+
+    def is_enabled(self):
+        return self.__enabled;
+
+    ############################################################################
+    ## Set/Get Callback                                                       ##
+    ############################################################################
+    def set_callback(self, callback):
+        self.__tick_callback = callback;
+
+    def get_callback(self):
+        return self.__tick_callback;
+
+    ############################################################################
+    ## Update                                                                 ##
+    ############################################################################
+    def update(self, dt):
+        if(not self.__enabled):
+            return;
+
+        #Update the timer...
+        self.__time_since_last_tick += dt;
+        if(self.__time_since_last_tick >= self.__tick_time):
+            self.__time_since_last_tick -= self.__tick_time;
+
+            #We have a valid callback?
+            assert self.__tick_callback is not None;
+            self.__tick_callback();
+
+
