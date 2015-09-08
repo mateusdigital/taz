ef08368ff5ca44707d9fdc78016d290c55e88780
Mateus Mesquita <n2o.matt@gmail.com>
2015-09-07 21:46:08 -0300

    Add the file that will hold the resources filenames.

---
+++ src/resources.py
@@ -0,0 +1,8 @@
+class Sprites:
+    #SplashScene...
+    AmazingCowLogo = "../resources/Sprite_AmazingCowLogo.png";
+    #MenuScene...
+    TazLogo       = "../resources/Sprite_TazLogo.png";
+    AmazingCowUrl = "../resources/Sprite_AmazingCowURL.png";
+    MenuPlay      = "../resources/Sprite_Play.png";
+    MenuCredits   = "../resources/Sprite_Credits.png";
