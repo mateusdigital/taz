af3c2527fae3478d4d4be3098407053c32f6f1e5
Mateus Mesquita <n2o.matt@gmail.com>
2015-09-11 10:56:31 -0300

    On Taz Class: Set some constants to remove the magic numbers. Add comments. Implement the stub for state management. Add the normal animation. Add the dead animation.
    On GameScen Class:
    Set some constants to remove magic numbers.
    Add some comments.
    Remove unneeded code.

--- src/game_scene.py
+++ src/game_scene.py
@@ -51,7 +51,7 @@ from clock      import BasicClock;
 from resources  import Sprites;
 import menu_scene; #To avoid circular imports;
 
-import pdb;
+import pdb; # :~)
 
 ################################################################################
 ## MovableObject                                                              ##
@@ -94,28 +94,91 @@ class MovableObject(Sprite):
 ## Taz                                                                        ##
 ################################################################################
 class Taz(Sprite):
-    TIME_TO_CHANGE_TRACK = 30;
-    HORIZONTAL_SPEED     = 500;
+    ############################################################################
+    ## Constants                                                              ##
+    ############################################################################
+    INITIAL_TRACK_INDEX  = 3;
+    INITIAL_POSITION_X   = 200;
 
+    SCALE_FACTOR     = 1.5;
+    HORIZONTAL_SPEED = 500;
+
+    TIME_TO_CHANGE_TRACK     = 40;
+    TIME_TO_NORMAL_ANIMATION = 150;
+    TIME_TO_DEATH_ANIMATION  = 1400;
+
+    STATE_ALIVE = 0;
+    STATE_DEAD  = 1;
+
+    ############################################################################
+    ## CTOR                                                                   ##
+    ############################################################################
     def __init__(self):
         Sprite.__init__(self);
 
-        self.load_image(Sprites.Taz);
+        self.__state = Taz.STATE_ALIVE;
 
-        x = 200;
-        y = GameScene.FIELD_TRACKS_Y[3];
+        #Set the Sprite vars.
+        self.__frames = [pygame.image.load(Sprites.TazFrame0),
+                         pygame.image.load(Sprites.TazFrame1)];
+        self.__current_frame = 0;
+        self.update_image(self.__frames[0]);
+
+        #Set the Track vars.
+        self.__current_track    = Taz.INITIAL_TRACK_INDEX;
+        self.__can_change_track = True;
+
+        #Set the Animation vars.
+        self.__sprite_is_flipped = False;
+
+        #Set the Initial Position.
+        x = Taz.INITIAL_POSITION_X;
+        y = GameScene.FIELD_TRACKS_Y[self.__current_track];
         self.set_position(x, y);
 
-        self.__current_track = 3;
-        self.__can_change_track = True;
+        #Initialize the Track Change timer.
         self.__change_track_timer = BasicClock(Taz.TIME_TO_CHANGE_TRACK);
-        # self.__change_track_timer.set_time();
         self.__change_track_timer.set_callback(self.on_change_track_timer_tick);
 
+        #Initialize the Normal Animation timer.
+        self.__normal_animation_timer = BasicClock(Taz.TIME_TO_NORMAL_ANIMATION);
+        self.__normal_animation_timer.set_callback(self.on_normal_animation_timer_tick);
+
+        #Initialize the Death Animation Timer.
+        self.__death_timer = BasicClock(Taz.TIME_TO_DEATH_ANIMATION);
+        self.__death_timer.set_callback(self.on_death_timer_tick);
+
+        #Start the Normal animation timer.
+        self.__normal_animation_timer.start();
+
+    ############################################################################
+    ## Time Callbacks                                                         ##
+    ############################################################################
     def on_change_track_timer_tick(self):
         self.__can_change_track = True;
         self.__change_track_timer.stop();
 
+    def on_normal_animation_timer_tick(self):
+        curr_frame_img = self.__frames[self.__current_frame];
+
+        #If Taz is dead we will stretch it a little bit.
+        #Otherwise just update the frame.
+        if(self.__state == Taz.STATE_DEAD):
+            scalled_w  = int(self.get_size_w() * Taz.SCALE_FACTOR);
+            self.image = pygame.transform.scale(curr_frame_img,
+                                                (scalled_w,
+                                                 self.get_size_h()));
+        else:
+            self.image = curr_frame_img;
+
+        self.__current_frame = (self.__current_frame + 1) % 2;
+
+    def on_death_timer_tick(self):
+        self.change_state_to_alive();
+
+    ############################################################################
+    ## Movement Functions                                                     ##
+    ############################################################################
     def change_track(self, delta):
         self.__current_track += delta;
         self.__change_track_timer.start();
@@ -129,20 +192,49 @@ class Taz(Sprite):
         self.set_position_y(GameScene.FIELD_TRACKS_Y[self.__current_track]);
 
     def move_horizontal(self, dir, dt):
+        #Move.
         speed = Taz.HORIZONTAL_SPEED * dir;
         self.move_x(speed * (dt / 1000.0));
 
+        #Constraint to field bounds...
         x = self.get_position_x();
-        # print x;
-        if(x <= 35): x = 35;
-        elif(x >= 409): x = 409;
+        if(x <= GameScene.FIELD_SOFT_LEFT):
+            x = GameScene.FIELD_SOFT_LEFT;
+        elif(x + self.get_size_w() >= GameScene.FIELD_SOFT_RIGHT):
+            x = GameScene.FIELD_SOFT_RIGHT - self.get_size_w();
         self.set_position_x(x);
 
-    def update(self, dt):
-        keys = pygame.key.get_pressed();
-        speed = 0;
+    ############################################################################
+    ## State Functions                                                        ##
+    ############################################################################
+    def change_state_to_alive(self):
+        print "Taz - Change State to Alive...";
+        self.__death_timer.stop();
+        self.__state = Taz.STATE_ALIVE;
 
+    def change_state_to_dead(self):
+        print "Taz - Change State to Dead...";
+        self.__death_timer.start();
+        self.__state = Taz.STATE_DEAD;
+
+    ############################################################################
+    ## Update                                                                 ##
+    ############################################################################
+    def update(self, dt):
+        #Get the state of keyboard.
+        keys = pygame.key.get_pressed();
+
+        #Update the timers...
         self.__change_track_timer.update(dt);
+        self.__normal_animation_timer.update(dt);
+        self.__death_timer.update(dt);
+
+        if(keys[pygame.locals.K_j]):
+            self.change_state_to_dead();
+
+        #If dead do not move.
+        if(self.__state == Taz.STATE_DEAD):
+            return;
 
         #Vertical Movement.
         if(self.__can_change_track):
@@ -154,19 +246,15 @@ class Taz(Sprite):
         if(keys[pygame.locals.K_RIGHT]): self.move_horizontal(+1, dt);
 
 
-
-
-
-
-
-
 ################################################################################
 ## GameScene                                                                  ##
 ################################################################################
 class GameScene(Scene):
-    FIELD_TRACKS_Y   = [62, 94, 126, 158, 190, 222, 254, 285];
-    FIELD_HARD_LEFT  = -32 + 10;
-    FIELD_HARD_RIGHT = 480 - 10;
+    FIELD_TRACKS_Y   = [62, 94, 126, 158, 190, 222, 254, 285]; #The field tracks.
+    FIELD_HARD_LEFT  = -32 + 10; #Right most positions when other stuff can go.
+    FIELD_HARD_RIGHT = 480 - 10; #Left  most positions when other stuff can go.
+    FIELD_SOFT_LEFT  = 33;   #Left  most position that Taz can go.
+    FIELD_SOFT_RIGHT = 440;  #Right most position that Taz can go.
 
     ############################################################################
     ## CTOR                                                                   ##
@@ -197,8 +285,8 @@ class GameScene(Scene):
         surface.fill((0,0,0));
         Scene.draw(self, surface);
 
-    def update(self, dt):
-        self.taz.update(dt);
+    # def update(self, dt):
+    #     self.taz.update(dt);
 
     def handle_events(self, event):
         #We're only interested in keydown events.
@@ -208,16 +296,8 @@ class GameScene(Scene):
         key = event.key;
         #Up and Down change the option.
         if(key == pygame.locals.K_ESCAPE):
-            exit(0);
-            # self.change_to_menu_scene();
-
-        # key = event.key;
-        # #Up and Down change the option.
-        # if(key == pygame.locals.K_UP or key == pygame.locals.K_DOWN):
-        #     self.change_menu_option();
-        # #Enter commit the selection.
-        # elif(key == pygame.locals.K_RETURN):
-        #     self.change_scene();
+            # exit(0);
+            self.change_to_menu_scene();
 
 
     def change_to_menu_scene(self):
