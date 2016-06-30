99dba2623891da9188002bb31f87a298d6347577
n2omatt <n2o.matt@gmail.com>
2016-06-30 06:10:43 -0300

    - Taz moves by now.

--- src/taz.py
+++ src/taz.py
@@ -41,202 +41,164 @@
 ##                                  Enjoy :)                                  ##
 ##----------------------------------------------------------------------------##
 
-## Imports ##
-#Pygame
-import pygame;
-#Project
-from clock                import BasicClock;
-from game_field_constants import GameFieldConstants;
-from resources            import Sprites;
-from scene                import Sprite;
+################################################################################
+## Imports                                                                    ##
+################################################################################
+## Pygame ##
+import pygame.locals;
+## NumPy ##
+import numpy;
+## Project ##
+import assets;
+import input;
+from cowclock import *;
 
-class Taz(Sprite):
+
+
+class Taz():
     ############################################################################
-    ## Constants                                                              ##
+    ## Constatns                                                              ##
     ############################################################################
-    INITIAL_TRACK_INDEX  = 3;
-    INITIAL_POSITION_X   = 200;
-
-    SCALE_FACTOR     = 1.5;
-    HORIZONTAL_SPEED = 400;
-
-    TIME_TO_CHANGE_TRACK     = 70;
-    TIME_TO_NORMAL_ANIMATION = 150;
-    TIME_TO_DEATH_ANIMATION  = 1400;
-
+    ## Public ##
     STATE_ALIVE = 0;
-    STATE_DEAD  = 1;
+    STATE_DYING = 1;
+    STATE_DEAD  = 2;
 
-    CONTROL_UP    = 0;
-    CONTROL_DOWN  = 1;
-    CONTROL_LEFT  = 2;
-    CONTROL_RIGHT = 3;
+    ## Private ##
+    _MAX_LIVES             = 3;
+    _FRAMES_COUNT          = 2;
+    _TRACKS_COUNT          = 8;
+    _ANIMATION_INVERVAL    = 0.15;
+    _CHANGE_TRACK_INTERVAL = 0.15; #COWTODO: TWEAK
+    _TRACK_OFFSET          = 32;
+    _SPEED                 = 300; #COWTODO: TWEAK
 
-    MAX_LIVES = 3
 
     ############################################################################
-    ## CTOR                                                                   ##
+    ## Init                                                                   ##
     ############################################################################
-    def __init__(self, death_animation_callback):
-        Sprite.__init__(self);
+    def __init__(self, min_bounds, max_bounds):
+        ## Housekeeping
+        self._state                    = Taz.STATE_ALIVE;
+        self._lives                    = Taz._MAX_LIVES;
+        self._eat_count                = 0;
 
-        ## iVars ##
-        self.__death_animation_callback = death_animation_callback;
-        self.__state                    = Taz.STATE_ALIVE;
-        self.__lives                    = Taz.MAX_LIVES;
-        self.__controls                 = [False, False, False, False];
-        self.__eat_count                = 0;
+        ## Frames / Animation
+        self._frames           = [];
+        self._animation_timer  = None;
 
-        #Set the Sprite vars.
-        self.__frames = [pygame.image.load(Sprites.Game_TazFrame0),
-                         pygame.image.load(Sprites.Game_TazFrame1)];
-        self.__current_frame = 0;
-        self.update_image(self.__frames[0]);
+        ## Movement / Bounds
+        self._curr_track_index   = 0;
+        self._can_change_track   = False;
+        self._position           = [min_bounds[0], min_bounds[1]];
+        self._min_bounds         = min_bounds;
+        self._max_bounds         = max_bounds;
+        self._track_change_timer = None;
 
-        #Set the Track vars.
-        self.__current_track    = GameFieldConstants.TAZ_INITIAL_TRACK_INDEX;
-        self.__can_change_track = True;
-
-        #Set the Initial Position.
-        self.reset_position();
-
-        #Initialize the Track Change timer.
-        self.__change_track_timer = BasicClock(Taz.TIME_TO_CHANGE_TRACK);
-        self.__change_track_timer.set_callback(self.__on_change_track_timer_tick);
-
-        #Initialize the Normal Animation timer.
-        self.__normal_animation_timer = BasicClock(Taz.TIME_TO_NORMAL_ANIMATION);
-        self.__normal_animation_timer.set_callback(self.__on_normal_animation_timer_tick);
-
-        #Initialize the Death Animation Timer.
-        self.__death_animation_timer = BasicClock(Taz.TIME_TO_DEATH_ANIMATION);
-        self.__death_animation_timer.set_callback(self.__on_death_animation_timer_tick);
-
-        #Start the Normal animation timer.
-        self.__normal_animation_timer.start();
+        ## Complete initialization.
+        self._init_frames();
+        self._init_timers();
 
 
     ############################################################################
     ## Public Functions                                                       ##
     ############################################################################
-    def set_controls(self, control_index, control_state):
-        self.__controls[control_index] = control_state;
-
-    def increment_eat_count(self):
-        self.__eat_count += 1;
+    ## Eat
+    def make_eat(self):
+        self._eat_count += 1;
 
     def get_eat_count(self):
-        return self.__eat_count;
+        return self._eat_count;
+
+    ## Lives
+    def kill(self):
+        pass;
 
     def get_lives(self):
-        return self.__lives;
+        return self._lives;
 
+    ## State
     def get_state(self):
-        return self.__state;
+        return self._state;
 
-    def set_dead(self):
-        self.__change_state_to_dead();
-
-    def reset_position(self):
-        self.set_position(GameFieldConstants.TAZ_INITIAL_POSITION_X,
-                          GameFieldConstants.TAZ_INITIAL_POSITION_Y);
 
     ############################################################################
-    ## Update                                                                 ##
+    ## Update / Draw                                                          ##
     ############################################################################
     def update(self, dt):
-        #Update the timers...
-        self.__change_track_timer.update(dt);
-        self.__normal_animation_timer.update(dt);
-        self.__death_animation_timer.update(dt);
+        ## Timers
+        self._animation_timer.update   (dt);
+        self._track_change_timer.update(dt);
+        print not self._track_change_timer.is_enabled();
 
-        #If dead do not move.
-        if(self.__state == Taz.STATE_DEAD):
-            return;
+        ## Movement
+        ## Vertical - Track Based
+        if(input.is_down(pygame.locals.K_DOWN) and self._change_track_is_ok()):
+            self._curr_track_index += 1;
+        elif(input.is_down(pygame.locals.K_UP) and self._change_track_is_ok()):
+            self._curr_track_index -= 1;
 
-        #Vertical Movement.
-        if(self.__can_change_track):
-            if(self.__controls[Taz.CONTROL_DOWN]): self.__change_track(+1);
-            if(self.__controls[Taz.CONTROL_UP  ]): self.__change_track(-1);
+        ## Horizontal - Pixel/sec Based
+        if(input.is_down(pygame.locals.K_LEFT)):
+            self._position[0] -= Taz._SPEED * dt;
+        elif(input.is_down(pygame.locals.K_RIGHT)):
+            self._position[0] += Taz._SPEED * dt;
 
-        #Horizontal Movement.
-        if(self.__controls[Taz.CONTROL_LEFT ]): self.__move_horizontal(-1, dt);
-        if(self.__controls[Taz.CONTROL_RIGHT]): self.__move_horizontal(+1, dt);
+        ## Maintain the Taz into GameField
+        self._curr_track_index = numpy.clip(self._curr_track_index,
+                                            0,
+                                            Taz._TRACKS_COUNT -1);
+
+        self._position[0] = numpy.clip(self._position  [0],
+                                       self._min_bounds[0],
+                                       self._max_bounds[0]);
 
 
-    ############################################################################
-    ## Time Callbacks                                                         ##
-    ############################################################################
-    def __on_change_track_timer_tick(self):
-        self.__can_change_track = True;
-        self.__change_track_timer.stop();
+        ## Update the vertical position based upon which track he is.
+        self._position[1] = self._curr_track_index * \
+                            Taz._TRACK_OFFSET      + \
+                            self._min_bounds[1];
 
-    def __on_normal_animation_timer_tick(self):
-        curr_frame_img = self.__frames[self.__current_frame];
 
-        #If Taz is dead we will stretch it a little bit.
-        #Otherwise just update the frame.
-        if(self.__state == Taz.STATE_DEAD):
-            scalled_w  = int(self.get_size_w() * Taz.SCALE_FACTOR);
-            self.image = pygame.transform.scale(curr_frame_img,
-                                                (scalled_w,
-                                                 self.get_size_h()));
-        else:
-            self.image = curr_frame_img;
-
-        self.__current_frame = (self.__current_frame + 1) % 2;
-
-    def __on_death_animation_timer_tick(self):
-        self.__death_animation_callback();
-        self.__change_state_to_alive();
+    def draw(self, surface):
+        index = self._animation_timer.get_tick_count() % Taz._FRAMES_COUNT;
+        surface.blit(self._frames[index], self._position);
 
 
     ############################################################################
-    ## Movement Functions                                                     ##
+    ## Private Methods                                                        ##
     ############################################################################
-    def __change_track(self, delta):
-        #Change the track, set that Taz cannot change track for
-        #a while start the timer to make Taz change track again.
-        self.__current_track   += delta;
-        self.__can_change_track = False;
-        self.__change_track_timer.start();
+    ## Inits ##
+    def _init_frames(self):
+        for i in xrange(0, Taz._FRAMES_COUNT):
+            self._frames.append(assets.load_image("TazFrame%d.png" %(i)));
 
-        #Keep Taz inside the Track bounds.
-        if(self.__current_track < 0):
-            self.__current_track = 0;
-
-        elif(self.__current_track >= GameFieldConstants.FIELD_TRACKS_LEN):
-            self.__current_track = GameFieldConstants.FIELD_TRACKS_LEN -1;
-
-        #Finally set the Y position to the track.
-        self.set_position_y(GameFieldConstants.FIELD_TRACKS_Y[self.__current_track]);
-
-    def __move_horizontal(self, dir, dt):
-        #Move.
-        speed = Taz.HORIZONTAL_SPEED * dir;
-        self.move_x(speed * (dt / 1000.0));
-
-        #Constraint to field bounds...
-        x = self.get_position_x();
-
-        if(x <= GameFieldConstants.FIELD_SOFT_LEFT):
-            x = GameFieldConstants.FIELD_SOFT_LEFT;
-
-        elif(x + self.get_size_w() >= GameFieldConstants.FIELD_SOFT_RIGHT):
-            x = GameFieldConstants.FIELD_SOFT_RIGHT - self.get_size_w();
-
-        self.set_position_x(x);
+        frame_size = self._frames[0].get_size();
+        self._max_bounds[0] -= frame_size[0];
+        self._max_bounds[1] -= frame_size[1];
 
 
-    ############################################################################
-    ## State Functions                                                        ##
-    ############################################################################
-    def __change_state_to_alive(self):
-        self.__death_animation_timer.stop();
-        self.__state = Taz.STATE_ALIVE;
+    def _init_timers(self):
+        ## Animation
+        self._animation_timer = CowClock(
+                                    time         = Taz._ANIMATION_INVERVAL,
+                                    repeat_count = CowClock.REPEAT_FOREVER,
+                                );
 
-    def __change_state_to_dead(self):
-        self.__death_animation_timer.start();
-        self.__state = Taz.STATE_DEAD;
-        self.__lives -= 1;
+        self._animation_timer.start();
 
+        ## Track Change
+        self._track_change_timer = CowClock(
+                                        time         = Taz._CHANGE_TRACK_INTERVAL,
+                                        repeat_count = 1
+                                   );
+
+
+    ## Movement
+    def _change_track_is_ok(self):
+        #COWTODO: Comment
+        is_ok = not self._track_change_timer.is_enabled();
+        if(is_ok):
+            self._track_change_timer.start();
+
+        return is_ok;
