6a866d2bf753abc360e0c25cccfe7c7610301420
n2omatt <n2o.matt@gmail.com>
2016-07-01 16:31:40 -0300

    - Remove old, unneeded stuff.

---
+++ src/enemy.py
@@ -0,0 +1,11 @@
+
+
+
+_FOOD_FRAME   = assets.load_image("Food.png"  );
+_BOMB_FRAME   = assets.load_image("Bomb.png"  );
+_CAUGHT_FRAME = assets.load_image("Caught.png");
+
+
+class Enemy:
+    def __init__(self, min_bounds, max_bounds):
+
