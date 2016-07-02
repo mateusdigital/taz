e490d8ef0a29d79cec44af64d5d9d068137d73b0
n2omatt <n2o.matt@gmail.com>
2016-07-01 21:19:22 -0300

    - Add enemies. - Add collision checking.

--- src/game_scene.py
+++ src/game_scene.py
@@ -54,6 +54,7 @@ import input;
 import position_helpers;
 from constants import *;
 from taz       import *;
+from enemy     import *;
 from hud       import *;
 
 
@@ -61,10 +62,17 @@ class GameScene():
     ############################################################################
     ## Constants                                                              ##
     ############################################################################
+    ## Private ##
     _STATE_PLAYING   = 0;
     _STATE_PAUSED    = 1;
     _STATE_GAME_OVER = 2;
 
+    _TRACKS_COUNT =  8;
+    _TRACK_OFFSET = 32;
+
+    _SCORE_MULTIPLIER   = 50;
+    _SPEED_UPDATE_COUNT = 20;
+    _SPEED_ACCELERATION = 20;
 
     ############################################################################
     ## CTOR                                                                   ##
@@ -81,27 +89,45 @@ class GameScene():
         self._game_field_pos = (GAME_WIN_CENTER_X - game_field_size[0] * 0.5,
                                 GAME_WIN_CENTER_Y - game_field_size[1] * 0.5);
 
+        #COWTODO: Remove the magic numbers.
+        #COWTODO: This initialization is tooooooooo verbose...
         game_field_size = self._game_field.get_size();
-        game_field_min = [self._game_field_pos[0] + 28,
-                          self._game_field_pos[1] + 32];
-        game_field_max = [self._game_field_pos[0] + game_field_size[0] - 28,
-                          self._game_field_pos[1] + game_field_size[1] - 32];
+
+        taz_field_min = [self._game_field_pos[0] + 28,
+                         self._game_field_pos[1] + GameScene._TRACK_OFFSET];
+        taz_field_max = [self._game_field_pos[0] + game_field_size[0] - 28,
+                         self._game_field_pos[1] + game_field_size[1] - GameScene._TRACK_OFFSET];
+
+        enemy_field_min = [self._game_field_pos[0],
+                           self._game_field_pos[1] + GameScene._TRACK_OFFSET];
+        enemy_field_max = [self._game_field_pos[0] + game_field_size[0] - 28,
+                           self._game_field_pos[1] + game_field_size[1]];
 
         ## Taz
-        #COWTODO: Remove the magic numbers.
         self._taz = Taz(
-            min_bounds  = game_field_min,
-            max_bounds  = game_field_max,
-            is_playable = True,
+            min_bounds   = taz_field_min,
+            max_bounds   = taz_field_max,
+            tracks_count = GameScene._TRACKS_COUNT,
+            track_offset = GameScene._TRACK_OFFSET,
+            is_playable  = True,
             dead_animation_callback = self._on_taz_dead_animation_done
         );
 
         ## Enemies
         self._enemies = [];
-
+        for i in xrange(0, GameScene._TRACKS_COUNT):
+            self._enemies.append(
+                Enemy(
+                    min_bounds   = enemy_field_min,
+                    max_bounds   = enemy_field_max,
+                    tracks_count = GameScene._TRACKS_COUNT,
+                    track_offset = GameScene._TRACK_OFFSET,
+                    track_index  = i
+                )
+            );
 
         ## Hud
-        self._hud = Hud(game_field_min, game_field_max);
+        self._hud = Hud(taz_field_min, taz_field_max);
 
 
     ############################################################################
@@ -123,12 +149,39 @@ class GameScene():
             return;
 
         ## Updates
-        self._taz.update(dt);
-        #COWTODO: Enemies...
-
         self._hud.update(dt);
-        ## Collisions
-        #COWTODO: Implement...
+        self._taz.update(dt);
+
+
+        ## Taz is Dying or dead - We don't need
+        ## update the Enemies anymore, them will be reseted
+        ## when taz animation is done.
+        if(self._taz.get_state() != Taz.STATE_ALIVE):
+            return;
+
+        taz_hit_box = self._taz.get_hit_box(); ## Need to be calc'ed 1 time...
+        for enemy in self._enemies:
+            enemy.update(dt);
+
+            ## Doesn't collided...
+            if(not enemy.check_collision(taz_hit_box)):
+                continue;
+
+            ## Collided
+            if(not enemy.is_fatal()):
+                self._taz.make_eat();
+                eat_count = self._taz.get_eat_count();
+                self._hud.set_score(
+                     eat_count * GameScene._SCORE_MULTIPLIER
+                );
+
+                ## Ate enough enmies to accelerate them...
+                if(eat_count % GameScene._SPEED_UPDATE_COUNT == 0 and \
+                    eat_count != 0):
+                    Enemy.Accelerate(GameScene._SPEED_ACCELERATION);
+
+            else:
+                self._taz.kill();
 
 
     def _update_paused(self, dt):
@@ -148,8 +201,12 @@ class GameScene():
     ############################################################################
     def draw(self, surface):
         surface.blit(self._game_field, self._game_field_pos);
+
         self._hud.draw(surface);
         self._taz.draw(surface);
+
+        for enemy in self._enemies:
+            enemy.draw(surface);
 
 
     ############################################################################
@@ -163,6 +220,8 @@ class GameScene():
             self._game_state = GameScene._STATE_GAME_OVER;
             return;
 
-        #COWTODO: Restart the enemies...
+        for enemy in self._enemies:
+            enemy.reset();
+
         self._taz.reset();
 
