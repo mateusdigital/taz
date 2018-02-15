b89620b1e6c04e2b2c8133873cfa5fbb945988bf
n2omatt <n2omatt@amazingcow.com>
2018-02-15 19:43:52 -0200

    - Update the setup script.

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,45 @@
+##~---------------------------------------------------------------------------##
+##                     _______  _______  _______  _     _                     ##
+##                    |   _   ||       ||       || | _ | |                    ##
+##                    |  |_|  ||       ||   _   || || || |                    ##
+##                    |       ||       ||  | |  ||       |                    ##
+##                    |       ||      _||  |_|  ||       |                    ##
+##                    |   _   ||     |_ |       ||   _   |                    ##
+##                    |__| |__||_______||_______||__| |__|                    ##
+##                             www.amazingcow.com                             ##
+##  File      : setup.py                                                      ##
+##  Project   : Game_Taz                                                      ##
+##  Date      : Feb 14, 2018                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : AmazingCow - 2018                                             ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
+
 ##----------------------------------------------------------------------------##
 ## Imports                                                                    ##
 ##----------------------------------------------------------------------------##
-import cx_Freeze
+import sys;
+import cx_Freeze;
 
 
 ##----------------------------------------------------------------------------##
 ## Vars                                                                       ##
 ##----------------------------------------------------------------------------##
-executables           = [cx_Freeze.Executable("main.py")];
 options_packages      = ["pygame", "numpy"];
-options_include_files = [];
+options_include_files = ["../assets"];
+options_excludes      =  ["tkinter", "tcl", "tk"];
+
+executables = [
+    cx_Freeze.Executable(
+        script = "main.py",
+        icon   = "../assets/icon.ico",
+        base   =  "Win32GUI" if sys.platform == "win32" else None
+    )
+];
+
 
 ##----------------------------------------------------------------------------##
 ## Script                                                                     ##
@@ -19,6 +49,7 @@ cx_Freeze.setup(
     options = {
         "build_exe" : {
             "packages"      : options_packages,
+            "excludes"      : options_excludes,
             "include_files" : options_include_files
         }
     },
